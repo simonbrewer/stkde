7862d984fabf0992f762d1a87ff80818b9233677
Simon Brewer <brewersi@gmail.com>
2019-01-27 16:30:34 -0700

    Added plot to caldates.R

--- caldates.R
+++ caldates.R
@@ -1,6 +1,7 @@
 ## Calibrate radiocarbon dates
 
 library(Bchron)
+library(ggplot2)
 
 ## Data
 dat = read.csv("data/SJco_14C_Dates_Draft_012419.csv")
@@ -11,3 +12,8 @@ caldates = BchronCalibrate(dat$Date, dat$Error,
                            calCurves=rep('intcal13',ndat),
                            ids=paste0('Date-',1:ndat))
 save(caldates, file="caldates.RData")
+
+## Sample plot
+plot.df = data.frame(ages = caldates$`Date-1`$ageGrid,
+                     density = caldates$`Date-1`$densities)
+ggplot(plot.df, aes(x=ages, y=density)) + geom_line() + theme_bw()
