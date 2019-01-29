acf2d6bf68e55796f79390d1a48a61abc2319a5f
Simon Brewer <brewersi@gmail.com>
2019-01-29 15:41:24 -0700

    Extracts time kernel for each sample

--- stkdeRnd.R
+++ stkdeRnd.R
@@ -11,7 +11,7 @@ library(rts)
 library(Bchron)
 
 ## How many samples?
-nbit = 100
+nbit = 10
 
 ## Window for analysis
 cnty = readOGR("ut_counties/Counties.shp")
@@ -46,66 +46,35 @@ sj.ppp <- ppp(dat.x, dat.y,
                    window=sj.owin)
 
 ## Space-time slices:
-tims <- seq(500,10500, by=500)
+tims <- seq(500, 10500, by=500)
 ntims = length(tims)
 
-## Parameters for kernel
-hlam <- c(2e4, 1e3)
+## Parameters for kernel: 1: spatial; 2: temporal
+hlam <- c(2e4, 1e3) ## Smooth
+hlam <- c(1e4, 500) ## Simplify
+
+## Parameters for kernel: 1: spatial; 2: temporal
+tims2 <- seq(500, 10500, by=50)
+tdens = matrix(NA, nrow = length(tims2), ncol = nbit)
 
 for (i in 1:nbit) {
+  print(paste("IT:",i))
   sj.den2 <- spattemp.density(sj.ppp, tt = caldates.rnd[i,],
                               h = hlam[1], lambda = hlam[2], 
                               tlim = c(0,12000), tres = 256)
-  sj.slice <- spattemp.slice(sj.den2,tt=tims)
+  tdens[,i] <- approx(sj.den2$temporal.z$x, sj.den2$temporal.z$y, tims2)$y
   
-  stop()
+  ## Get time slices
+  sj.slice1 <- spattemp.slice(sj.den2, tt=tims)
+  
+  if (i == 1) {
+    sj.slice = sj.slice1$z.cond
+  } else {
+    for (j in 1:ntims) {
+      sj.slice[[j]] = sj.slice[[j]] + sj.slice1$z.cond[[j]]
+    }
+  }
+  
 }
-
 
 stop()
-## Space time density with default settings
-sj.den1 <- spattemp.density(sj.ppp,tres=500) 
-
-## Plot and compare 
-tims <- c(2000,4000,6000)
-par(mfcol=c(2,3))
-for(i in tims){ 
-  plot(sj.den1,i,override.par=FALSE,fix.range=TRUE,main=paste("joint",i))
-  plot(sj.den1,i,"conditional",override.par=FALSE,main=paste("cond.",i))
-}
-
-## Likelihood based parameter estimates
-hlam <- LIK.spattemp(sj.ppp,tlim=c(0,11000),verbose=FALSE)
-print(hlam)
-sj.den2 <- spattemp.density(sj.ppp,h=hlam[1],lambda=hlam[2], 
-                            tlim=c(0,11000),tres=256)
-
-## Modified parameters for visualization
-sj.den2 <- spattemp.density(sj.ppp,h=2e4,lambda=1000, 
-                            tlim=c(0,11000),tres=256)
-
-tims <- c(2000,4000,6000,8000,10000)
-par(mfcol=c(2,3))
-for(i in tims){ 
-  plot(sj.den2,i,"conditional",override.par=FALSE,main=paste("cond.",i))
-}
-
-## Space-time slices:
-tims <- seq(500,10500, by=500)
-ntims = length(tims)
-f.slice <- spattemp.slice(sj.den2,tt=tims)
-
-## Output to netcdf (not working brilliantly if I'm honest)
-for(i in 1:ntims){   
-  dens.r = raster(f.slice$z.cond[[i]])
-  if (i == 1) {
-    dens.stk = stack(dens.r)
-  } else {
-    dens.stk = stack(dens.stk, dens.r)
-  }
-}
-
-writeRaster(dens.stk, "dens.nc", format="netCDF", 
-            varname = "lambda", overwrite=TRUE, bylayer=FALSE)
-save(sj.den2, file="sj.den2.RData")
-# writeRaster(dens.stk, "dens.grd")
